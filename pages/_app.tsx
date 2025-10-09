36d9a73f413ddbe45b78a40974ac9f16c0c9e350
skufig <31157024+skufig@users.noreply.github.com>
2025-10-09 13:52:02 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 // pages/_app.tsx (пример добавления Footer)
+import "regenerator-runtime/runtime";
 import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
